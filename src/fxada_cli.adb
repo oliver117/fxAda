5e5e31a971165d9b76b0e3674a2f962b9052f1ad
Oliver Kleinke <oliver@c-01a.de>
2013-12-19 00:29:31 +0100

    Implements simple parsing of command line parameters.

--- src/fxada_cli.adb
+++ src/fxada_cli.adb
@@ -20,96 +20,110 @@
 --  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 with Ada.Calendar.Formatting;
-with Ada.Characters.Latin_1;  use Ada.Characters;
+with Ada.Characters.Latin_1;
+with Ada.Text_IO;
 with Ada.Command_Line;
 with Ada.Strings.Unbounded;
-with Ada.Text_IO;
 
-with Oanda_API;
+with GNAT.Command_Line;
+with GNAT.Directory_Operations;
 with Oanda_API.Rates;
 
 procedure fxAda_CLI is
-   use Ada.Text_IO;
-   use Oanda_API;
+   Timeframe : Oanda_API.Rates.Granularity_T;
+   Timeframe_Set : Boolean := False;
 
+   Instrument : Oanda_API.Instrument_T;
+   Instrument_Set : Boolean := False;
+
+   Exe_Name : constant String :=
+     GNAT.Directory_Operations.Base_Name(Ada.Command_Line.Command_Name) & ": ";
 begin
+   -- parse command line
+   declare
+      use GNAT.Command_Line;
+      use Ada.Characters;
 
-   if Ada.Command_Line.Argument_Count = 0 then
+      Config : Command_Line_Configuration;
+
+      procedure Getopt_Callback (Switch, Param, Section: String) is
+      begin
+         if Switch = "-i" then
+            Instrument := Oanda_API.To_Instrument (Param);
+            Instrument_Set := True;
+         elsif Switch = "-t" then
+            Timeframe := Oanda_API.Rates.Granularity_T'Value(Param);
+            Timeframe_Set := True;
+         end if;
+      end Getopt_Callback;
+
+   begin
+      Define_Switch (Config, "-i:", Long_Switch => "--instrument=",
+                     Help => "Selects the instrument. Accepts any instrument identifier.");
+      Define_Switch (Config, "-t:", Long_Switch => "--timeframe=",
+                  Help => "Sets the Timeframe. Possible values:" & Latin_1.LF &
+                    Latin_1.HT & "S5, S10, S15, S30," & Latin_1.LF &
+                    Latin_1.HT & "M1, M2, M3, M5, M10, M15, M30," & Latin_1.LF &
+                    Latin_1.HT & "H1, H2, H3, H4, H6, H8, H12, D, W or M.");
+
+      Getopt (Config, Getopt_Callback'Unrestricted_Access); -- ugly, fix
+
+
+   exception
+      when GNAT.Command_Line.Invalid_Switch =>
+         null;
+      when GNAT.Command_Line.Invalid_Parameter =>
+         Ada.Text_IO.Put_Line (Exe_Name & "invalid parameter for option '" & Full_Switch & "'");
+   end;
+
+   -- instrument is specified => get quote
+   if Instrument_Set then
+      -- timeframe is specified => get candlestick data
+      if Timeframe_Set then
+         declare
+            use Oanda_API;
+            use Oanda_API.Rates;
+
+            History : constant Candlestick_Array := Get_History (Instrument    => Instrument,
+                                                                 Granularity   => Timeframe,
+                                                                 Count         => 1,
+                                                                 Candle_Format => Midpoint,
+                                                                 Include_First => True);
+            H : constant Candlestick := History (History'First);
+         begin
+            Ada.Text_IO.Put_Line ("Time: " & Ada.Calendar.Formatting.Image (H.Time));
+            Ada.Text_IO.Put_Line ("Open: " & Rate'Image (H.Open_Mid));
+            Ada.Text_IO.Put_Line ("High: " & Rate'Image (H.High_Mid));
+            Ada.Text_IO.Put_Line ("Low: " & Rate'Image (H.Low_Mid));
+            Ada.Text_IO.Put_Line ("Close: " & Rate'Image (H.Close_Mid));
+         end;
+      else -- timeframe is not specified => get simple quote
+         declare
+            use Oanda_API;
+            use Oanda_API.Rates;
+
+            Q : constant Quote := Get_Quote (Instrument);
+         begin
+            Ada.Text_IO.Put_Line ("Time: " & Ada.Calendar.Formatting.Image (Q.Time));
+            Ada.Text_IO.Put_Line ("Bid: " & Rate'Image (Q.Bid));
+            Ada.Text_IO.Put_Line ("Ask: " & Rate'Image (Q.Ask));
+         end;
+      end if;
+   else  -- instrument is not specified => get instrument list
       declare
-         Instruments : constant Instrument_Array := Rates.Get_Instruments (Oanda_API.Test_Account);
+         use Ada.Characters.Latin_1;
+         use Oanda_API;
+         use Oanda_API.Rates;
+         Instruments : constant Instrument_Information_Array := Get_Instrument_Information (Oanda_API.Test_Account);
       begin
          for I in Instruments'Range loop
-            Put_Line
-              ("Identifier: " &
-               To_String (Instruments (I).Identifier) &
-               Latin_1.HT &
-               "Display Name: " &
-               Ada.Strings.Unbounded.To_String (Instruments (I).Display_Name));
+            Ada.Text_IO.Put_Line ("Name: " & Ada.Strings.Unbounded.To_String (Instruments (I).Display_Name)
+                                  & ", ID: " & To_String (Instruments (I).Instrument));
          end loop;
       end;
-   elsif Ada.Command_Line.Argument_Count = 1
-     and then Ada.Command_Line.Argument (1) /= "help"
-   then
-      declare
-         Instr_Ident : constant Instrument_Identifier :=
-           To_Identifier (Ada.Command_Line.Argument (1));
-         Instr : constant Instrument := (Identifier => Instr_Ident, others => <>);
-         Q : constant Rates.Quote := Rates.Get_Quote (Instr);
-      begin
-         Put_Line
-           ("Instrument: " &
-            To_String (Q.Instrument) &
-            Latin_1.HT &
-            "Bid: " &
-            Rate'Image (Q.Bid) &
-            Latin_1.HT &
-            "Ask: " &
-            Rate'Image (Q.Ask) &
-            Latin_1.HT &
-            "Time: " &
-            Ada.Calendar.Formatting.Image (Q.Time) &
-            Latin_1.HT &
-            "Halted: " &
-            Boolean'Image (Q.Halted));
-      end;
-   elsif Ada.Command_Line.Argument_Count = 3
-     and then Ada.Command_Line.Argument (1) = "candles"
-   then
-      declare
-         Sticks : constant Rates.Candlestick_Array :=
-           Rates.Get_History
-              (Instrument    =>
-                  To_Identifier (Ada.Command_Line.Argument (3)),
-               Granularity   =>
-                  Rates.Granularity_T'Value
-                    (Ada.Command_Line.Argument (2)),
-               Count         => 10,
-               Start_Time    => No_Time,
-               End_Time      => No_Time,
-               Candle_Format => Rates.Midpoint);
-      begin
-         for I in Sticks'Range loop
-            Put_Line
-              ("Open: " &
-               Rate'Image (Sticks (I).Open_Mid) &
-               Latin_1.HT &
-               "High: " &
-               Rate'Image (Sticks (I).High_Mid) &
-               Latin_1.HT &
-               "Low: " &
-               Rate'Image (Sticks (I).Low_Mid) &
-               Latin_1.HT &
-               "Close: " &
-               Rate'Image (Sticks (I).Close_Mid) &
-               Latin_1.HT &
-               " Time: " &
-               Ada.Calendar.Formatting.Image (Sticks (I).Time));
-         end loop;
-      end;
-   else
-      Ada.Text_IO.Put_Line ("Usage:");
-      Ada.Text_IO.Put_Line ("   fxada_cli [instrument]");
-      Ada.Text_IO.Put_Line ("or");
-      Ada.Text_IO.Put_Line ("   fxada_cli candles [timeframe instrument]");
    end if;
+exception
+   when GNAT.Command_Line.Exit_From_Command_Line =>
+      null;
+
 end fxAda_CLI;
