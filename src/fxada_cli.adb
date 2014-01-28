1b12ff668f0f1ca32c29813b9ce60b07e7e294af
Oliver Kleinke <oliver@c-01a.de>
2014-01-28 15:26:41 +0100

    Adapts to new OandaAPI organization.

--- src/fxada_cli.adb
+++ src/fxada_cli.adb
@@ -21,9 +21,10 @@
 
 with Ada.Calendar.Formatting;
 with Ada.Characters.Latin_1;
-with Ada.Text_IO;
 with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Strings.Unbounded;
+with Ada.Text_IO;
 
 with GNAT.Command_Line;
 with GNAT.Directory_Operations;
@@ -31,9 +32,10 @@ with Oanda_API.Rates;
 
 procedure fxAda_CLI is
    Timeframe : Oanda_API.Rates.Granularity_T;
-   Timeframe_Set : Boolean := False;
-
    Instrument : Oanda_API.Instrument_T;
+
+   Timeframe_Set : Boolean := False;
+   Information_Set : Boolean := False;
    Instrument_Set : Boolean := False;
 
    Exe_Name : constant String :=
@@ -49,25 +51,44 @@ begin
       procedure Getopt_Callback (Switch, Param, Section: String) is
       begin
          if Switch = "-i" then
-            Instrument := Oanda_API.To_Instrument (Param);
-            Instrument_Set := True;
+            Information_Set := True;
          elsif Switch = "-t" then
-            Timeframe := Oanda_API.Rates.Granularity_T'Value(Param);
-            Timeframe_Set := True;
+            begin
+               Timeframe := Oanda_API.Rates.Granularity_T'Value(Param);
+               Timeframe_Set := True;
+            exception
+               when Constraint_Error =>
+                  Ada.Text_IO.Put_Line ("Invalid parameter: " & Param);
+                  raise Exit_From_Command_Line;
+            end;
          end if;
       end Getopt_Callback;
 
    begin
-      Define_Switch (Config, "-i:", Long_Switch => "--instrument=",
-                     Help => "Selects the instrument. Accepts any instrument identifier.");
+      Define_Switch (Config, "-i", Long_Switch => "--instrument-information",
+                     Argument => "IDENTIFIER",
+                     Help => "Displays information for the given instrument.");
       Define_Switch (Config, "-t:", Long_Switch => "--timeframe=",
-                  Help => "Sets the Timeframe. Possible values:" & Latin_1.LF &
-                    Latin_1.HT & "S5, S10, S15, S30," & Latin_1.LF &
-                    Latin_1.HT & "M1, M2, M3, M5, M10, M15, M30," & Latin_1.LF &
-                    Latin_1.HT & "H1, H2, H3, H4, H6, H8, H12, D, W or M.");
+                     Argument => "GRANULARITY",
+                     Help => "Sets the Timeframe granularity. Possible values:" & Latin_1.LF &
+                     Latin_1.HT & "S5, S10, S15, S30," & Latin_1.LF &
+                     Latin_1.HT & "M1, M2, M3, M5, M10, M15, M30," & Latin_1.LF &
+                       Latin_1.HT & "H1, H2, H3, H4, H6, H8, H12, D, W or M.");
+
+      Set_Usage (Config,
+                 Usage    => "[switches] instrument");
 
       Getopt (Config, Getopt_Callback'Unrestricted_Access); -- ugly, fix
 
+      declare
+         Arg : constant String := Get_Argument;
+      begin
+         if Arg /= "" then
+            Instrument := Oanda_API.To_Instrument (Arg);
+            Instrument_Set := True;
+         end if;
+         -- exception
+      end;
 
    exception
       when GNAT.Command_Line.Invalid_Switch =>
@@ -76,10 +97,26 @@ begin
          Ada.Text_IO.Put_Line (Exe_Name & "invalid parameter for option '" & Full_Switch & "'");
    end;
 
-   -- instrument is specified => get quote
+   -- instrument specified? => instrument specific operations
    if Instrument_Set then
-      -- timeframe is specified => get candlestick data
-      if Timeframe_Set then
+      if Information_Set then -- display information
+         declare
+            use Ada.Text_IO;
+            use Oanda_API;
+
+            I : constant Instrument_Information := Rates.Get_Instrument_Information (Test_Account, Instrument);
+         begin
+            Put_Line (Ada.Strings.Unbounded.To_String (I.Display_Name));
+            Put_Line ("----------");
+            Put_Line ("Identifier: " & To_String (I.Instrument));
+            Put_Line ("Pip: " & Rate'Image (I.Pip));
+            Put_Line ("Maximum trade size: " & Positive'Image (I.Max_Trade_Units));
+            Put_Line ("Precision: " & Rate'Image (I.Precision));
+            Put_Line ("Maximum trailing stop: " & Pips'Image (I.Max_Trailing_Stop));
+            Put_Line ("Minimum trailing stop: " & Pips'Image (I.Min_Trailing_Stop));
+            Put_Line ("Margin rate: " & Margin_Rate_T'Image (I.Margin_Rate));
+         end;
+      elsif Timeframe_Set then -- display historic quotes / candlestick data
          declare
             use Oanda_API;
             use Oanda_API.Rates;
@@ -97,7 +134,7 @@ begin
             Ada.Text_IO.Put_Line ("Low: " & Rate'Image (H.Low_Mid));
             Ada.Text_IO.Put_Line ("Close: " & Rate'Image (H.Close_Mid));
          end;
-      else -- timeframe is not specified => get simple quote
+      else -- get a simple quote
          declare
             use Oanda_API;
             use Oanda_API.Rates;
@@ -109,7 +146,7 @@ begin
             Ada.Text_IO.Put_Line ("Ask: " & Rate'Image (Q.Ask));
          end;
       end if;
-   else  -- instrument is not specified => get instrument list
+   else -- no instrument specified => display list of instruments
       declare
          use Ada.Characters.Latin_1;
          use Oanda_API;
@@ -125,5 +162,8 @@ begin
 exception
    when GNAT.Command_Line.Exit_From_Command_Line =>
       null;
-
+   when Oanda_API.No_Such_Instrument =>
+      Ada.Text_IO.Put_Line ("No such instrument; please specify a valid instrument identifier.");
+   when Error : Oanda_API.API_Error =>
+      Ada.Text_IO.Put_Line ("API error: " &  Ada.Exceptions.Exception_Message (Error));
 end fxAda_CLI;
